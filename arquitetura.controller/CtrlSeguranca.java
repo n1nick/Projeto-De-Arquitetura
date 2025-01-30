996cb06750fc854e94eb8792d21b94fecf9f4518
nini <113205557+n1nick@users.noreply.github.com>
2025-01-29 23:15:58 -0300

    Create CtrlSeguranca.java

---
+++ arquitetura.controller/CtrlSeguranca.java
@@ -0,0 +1,12 @@
+package arquitetura.controller;
+
+import arquitetura.model.Seguranca;
+
+public class CtrlSeguranca {
+
+	public static boolean alterarSenha(Seguranca seguranca) {
+		// TODO Auto-generated method stub
+		return true;
+	}
+
+}

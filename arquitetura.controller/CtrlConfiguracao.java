1f8b1d180920be1ed6bfd3e91a6d461a481b074c
nini <113205557+n1nick@users.noreply.github.com>
2025-01-29 23:14:44 -0300

    Create CtrlConfiguracao.java

---
+++ arquitetura.controller/CtrlConfiguracao.java
@@ -0,0 +1,22 @@
+package arquitetura.controller;
+
+import arquitetura.model.Configuracao;
+
+public class CtrlConfiguracao {
+    
+    public static boolean alterarIdioma(Configuracao configuracao) {
+        // Implementar lógica de alteração de idioma
+        return true;
+    }
+    
+    public static boolean alterarTema(Configuracao configuracao) {
+        // Implementar lógica de alteração de tema
+        return true;
+    }
+    
+    public static boolean excluirConta(Configuracao configuracao) {
+        // Implementar lógica de exclusão de conta
+        return true;
+    }
+    
+}

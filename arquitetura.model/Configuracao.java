056dd3406c8ea52105c2747e3ee4b57aedf67559
nini <113205557+n1nick@users.noreply.github.com>
2025-01-29 23:09:19 -0300

    Create Configuracao.java

---
+++ arquitetura.model/Configuracao.java
@@ -0,0 +1,24 @@
+package arquitetura.model;
+
+public class Configuracao {
+    
+    private String idioma;
+    private String tema;
+    
+    public String getIdioma() {
+        return idioma;
+    }
+    
+    public void setIdioma(String idioma) {
+        this.idioma = idioma;
+    }
+    
+    public String getTema() {
+        return tema;
+    }
+    
+    public void setTema(String tema) {
+        this.tema = tema;
+    }
+    
+}

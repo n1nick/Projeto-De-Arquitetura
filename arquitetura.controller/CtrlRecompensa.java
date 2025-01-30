a731bd2bc940da23f177c61bc0fada186e75883d
nini <113205557+n1nick@users.noreply.github.com>
2025-01-29 23:15:33 -0300

    Create CtrlRecompensa.java

---
+++ arquitetura.controller/CtrlRecompensa.java
@@ -0,0 +1,17 @@
+package arquitetura.controller;
+
+import arquitetura.model.Recompensa;
+
+public class CtrlRecompensa {
+    
+    public static boolean concederRecompensa(Recompensa recompensa) {
+        // Implementar lógica de concessão de recompensa
+        return true;
+    }
+
+	public static boolean criarRecompensa(Recompensa recompensa) {
+		// TODO Auto-generated method stub
+		return true;
+	}
+    
+}
